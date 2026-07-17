9e785e1002b7d2e5de7721a4373f93ca83c4161d
Sreehari-M-2002 <msreeharivenugopal2002@gmail.com>
2026-07-17 21:06:35 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,url_for,redirect
+from models import db,Student
+
+
 app = Flask(__name__)
 #above are configuration setups
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 @app.route('/')  #end point
 def home():      #function
@@ -19,6 +27,29 @@ def loop():
     subjects = ['flask', 'flaskapi', 'django']
 
     return render_template('loop.html',subjects=subjects)
+
+@app.route('/add_students', methods=['GET','POST'])
+def Add_students():
+    
+    if request.method=='POST':
+
+        name=request.form.get('name')
+        desc = request.form.get('desc')
+
+        # print(name)
+        # print(desc)
+
+        student= Student(
+            name=name,
+            desc=desc
+        )
+
+        db.session.add(student)
+        db.session.commit('desc')
+        return redirect(url_for('Add_Students'))
+    
+    get_student = Student.query.all()
+    return render_template('students_sheet.html',students=get_student)
 
 
 #below are configuration setups
