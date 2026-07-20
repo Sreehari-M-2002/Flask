4163f52e67acaf737e3778ea67d96256a7b528d8
Sreehari-M-2002 <msreeharivenugopal2002@gmail.com>
2026-07-20 21:29:10 +0530

    update

--- app.py
+++ app.py
@@ -10,9 +10,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+# <-----------HOME---------->
+
+
 @app.route('/')  #end point
 def home():      #function
     return render_template('home.html')
+
+
+#<---------------USER---------->
 
 @app.route('/user')
 def user():
@@ -22,36 +28,77 @@ def user():
     marks = 60
     return render_template('user.html', name=name, position=position, marks=marks)
 
+#<------------LOOP--------------->
+
 @app.route('/loop')
 def loop():
     subjects = ['flask', 'flaskapi', 'django']
 
     return render_template('loop.html',subjects=subjects)
 
-@app.route('/add_students', methods=['GET','POST'])
-def Add_students():
-    
-    if request.method=='POST':
 
-        name=request.form.get('name')
+#<-----------------ADD STUDENTS----------------->
+
+
+@app.route('/add_students', methods=['GET', 'POST'])
+def Add_students():
+
+    if request.method == 'POST':
+
+        name = request.form.get('name')
         desc = request.form.get('desc')
 
-        # print(name)
-        # print(desc)
-
-        student= Student(
+        student = Student(
             name=name,
             desc=desc
         )
 
         db.session.add(student)
-        db.session.commit('desc')
-        return redirect(url_for('Add_Students'))
-    
-    get_student = Student.query.all()
-    return render_template('students_sheet.html',students=get_student)
+        db.session.commit()
 
+        return redirect(url_for('Add_students'))
+
+    get_student = Student.query.all()
+    return render_template('students_sheet.html', students=get_student)
+
+#<---------------------DELETE STUDENTS---------------->
+
+@app.route('/delete_students/<int:id>/')
+def remove_student(id):
+
+    student = Student.query.get(id)
+
+    db.session.delete(student)
+    db.session.commit()
+
+    return redirect(url_for('Add_students'))
+
+#<--------------UPDATE------------->
+
+@app.route('/alter_student/<int:id>/', methods=['GET', 'POST'])
+def update_student(id):
+
+    student = Student.query.get(id)
+
+    if request.method == 'POST':
+        student.name = request.form.get('name')
+        student.desc = request.form.get('desc')
+        db.session.commit()
+        return redirect(url_for('Add_students'))
+
+    return render_template('update.html', student=student)
+
+#<---------- Single student search -------->
+
+@app.route('/full_detail/<int:id>/')
+def full_detail(id):
+
+    student = Student.query.get(id)
+    return render_template('full_detail.html',student=student)
+
+#<------------------------------------------->
 
 #below are configuration setups
+
 if __name__ == '__main__':
     app.run(debug=True)
