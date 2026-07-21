eeeb46b38b1e0faf1c4a1d95d58826d5681caf21
Sreehari-M-2002 <msreeharivenugopal2002@gmail.com>
2026-07-21 21:16:30 +0530

    updated code

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request,url_for,redirect
-from models import db,Student
+from flask import Flask, render_template, request,url_for,redirect, session
+from models import db,Student, User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
 #above are configuration setups
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db'
+app.config['SECRET_KEY'] = 'authenticationkey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-
-# <-----------HOME---------->
-
 
 @app.route('/')  #end point
 def home():      #function
@@ -36,12 +35,78 @@ def loop():
 
     return render_template('loop.html',subjects=subjects)
 
+#<-----------------SignUp Page-------------------------->
+
+@app.route('/signup', methods=['GET', 'POST'])
+def Register():
+
+    if request.method == 'POST':
+
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        hashed_password = generate_password_hash(password)
+
+        user = User(
+            fname=fname,
+            lname=lname,
+            username=username,
+            email=email,
+            password=hashed_password
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('Login'))
+
+    return render_template('signup.html')
+
+#<------------- LogIN Page------------->
+
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        
+        if user and check_password_hash(user.password,password):
+            
+            session['user_id'] = user.id
+
+            return redirect(url_for('Add_students'))
+
+    return render_template('login.html')
+
+
+#<-------------Log Out ------------->
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id',None)
+    return redirect(url_for('Login'))
+
+#@login_required = login_url('login')
+
+
+
 
 #<-----------------ADD STUDENTS----------------->
 
 
 @app.route('/add_students', methods=['GET', 'POST'])
 def Add_students():
+
+    if 'user_id' not in session:
+        return redirect(url_for('Login'))
+
 
     if request.method == 'POST':
 
@@ -96,9 +161,13 @@ def full_detail(id):
     student = Student.query.get(id)
     return render_template('full_detail.html',student=student)
 
-#<------------------------------------------->
 
-#below are configuration setups
+
+
+
+
+
+#<--------below are configuration setups-------->
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -5,3 +5,11 @@ class Student(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(20),nullable=False)
     desc= db.Column(db.String(20),nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable =False)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    fname = db.Column(db.String(20), nullable=False)
+    lname = db.Column(db.String(20), nullable = False)
